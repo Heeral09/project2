406774e303e2ddcd9a7478253e0e7179babad573
Heeral Patel <hp336@afsconnect1.njit.edu>
2017-12-21 21:10:08 -0500

    base folders

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'view/header.php'; ?>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="login.php">Login</a>
+        </li>
+        <li>
+            <a href="signup.php">Register</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title> Todo list</title>
+    <link rel="stylesheet" type="text/css"
+          href="http://web.njit.edu/~hp336/is218project2/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My Guitar Shop</h1></header>
